c2ee2b25f522d6cdc0e5368127787f9ee47e2b3b
Michael Lazebny <miskadl09@gmail.com>
2025-10-24 11:57:27 +0200

    feat: add image upscaling functionality and related assets

--- packages/fast_image/bin/rust_upscale.dart
+++ packages/fast_image/bin/rust_upscale.dart
@@ -1,8 +1,9 @@
 import 'package:fast_image/fast_image.dart';
 
 void main() {
+  final stopwatch = Stopwatch()..start();
   final image = FastImage.fromFile('assets/img/example_img.jpg');
   final upscaledImage = image.resize(3840, 2160);
-
   upscaledImage.saveToFile('upscaled_image.jpg');
+  print('Image upscaled in ${stopwatch.elapsedMilliseconds} ms');
 }
